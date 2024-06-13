851acfba2ed6fdfc4269fbcf8e6ae51f26310588
Matheus Meissner <matheus.iembo@gmail.com>
2024-06-13 16:33:58 -0300

    adiciona_informatvo_ao_cursor

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,22 +1,8 @@
-document.getElementById('download-form').addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const url = document.getElementById('url').value;
-    const response = await fetch('/download', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url: url })
-    });
-    const data = await response.json();
-    const message = document.getElementById('message');
-    message.innerHTML = data.message;
-});
-
-document.getElementById('url').addEventListener('input', async function () {
-    const url = this.value;
-    if (url) {
-        const response = await fetch('/get_thumbnail', {
+window.onload = function() {
+    document.getElementById('download-form').addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const url = document.getElementById('url').value;
+        const response = await fetch('/download', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -24,20 +10,52 @@ document.getElementById('url').addEventListener('input', async function () {
             body: JSON.stringify({ url: url })
         });
         const data = await response.json();
-        const thumbnail = document.getElementById('thumbnail');
-        thumbnail.src = data.thumbnail_url;
-        thumbnail.style.display = 'block';
-        thumbnail.setAttribute('data-url', url); // Defina o atributo data-url com a URL do vídeo
-    }
-});
+        const message = document.getElementById('message');
+        message.innerHTML = data.message;
+    });
 
-document.getElementById('thumbnail').addEventListener('click', function () {
-    const videoUrl = this.getAttribute('data-url');
-    if (videoUrl) {
-        window.open(videoUrl, '_blank'); // Abre a URL do vídeo em uma nova aba
-    }
-});
+    document.getElementById('url').addEventListener('input', async function () {
+        const url = this.value;
+        if (url) {
+            const response = await fetch('/get_thumbnail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ url: url })
+            });
+            const data = await response.json();
+            const thumbnail = document.getElementById('thumbnail');
+            thumbnail.src = data.thumbnail_url;
+            thumbnail.style.display = 'block';
+            thumbnail.setAttribute('data-url', url); // Defina o atributo data-url com a URL do vídeo
+        }
+    });
 
-document.getElementById('logo').addEventListener('click', function () {
-    window.location.reload();
-});
+    document.getElementById('thumbnail').addEventListener('click', function () {
+        const videoUrl = this.getAttribute('data-url');
+        if (videoUrl) {
+            window.open(videoUrl, '_blank'); // Abre a URL do vídeo em uma nova aba
+        }
+    });
+
+    document.getElementById('logo').addEventListener('click', function () {
+        window.location.reload();
+    });
+
+    const thumbnail = document.getElementById('thumbnail');
+    const tooltip = document.getElementById('tooltip');
+
+    thumbnail.addEventListener('mouseover', function () {
+        tooltip.style.display = 'block';
+    });
+
+    thumbnail.addEventListener('mousemove', function (e) {
+        tooltip.style.left = e.pageX + 10 + 'px'; // Ajuste a posição horizontal
+        tooltip.style.top = e.pageY + 10 + 'px'; // Ajuste a posição vertical
+    });
+
+    thumbnail.addEventListener('mouseout', function () {
+        tooltip.style.display = 'none';
+    });
+}
