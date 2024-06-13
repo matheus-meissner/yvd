56c395a920381219166b5e9a2cdb53f61e8c9008
Matheus Meissner <matheus.iembo@gmail.com>
2024-06-13 16:22:38 -0300

    torna_thumb_clicavel
    direciona usuario para o youtube

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -27,6 +27,14 @@ document.getElementById('url').addEventListener('input', async function () {
         const thumbnail = document.getElementById('thumbnail');
         thumbnail.src = data.thumbnail_url;
         thumbnail.style.display = 'block';
+        thumbnail.setAttribute('data-url', url); // Defina o atributo data-url com a URL do vídeo
+    }
+});
+
+document.getElementById('thumbnail').addEventListener('click', function () {
+    const videoUrl = this.getAttribute('data-url');
+    if (videoUrl) {
+        window.open(videoUrl, '_blank'); // Abre a URL do vídeo em uma nova aba
     }
 });
 
