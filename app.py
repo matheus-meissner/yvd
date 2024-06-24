5227ad32cb710457af81618a0302d8195938456c
Matheus Meissner <matheus.iembo@gmail.com>
2024-06-24 10:16:11 -0300

    TESTE_DEPLOY_12

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-from pytube import YouTube
-from flask import Flask, request, send_file, render_template
-import os
-import tempfile
-
-app = Flask(__name__)
-
-def download_video(url):
+def download_video(url, path):
     yt = YouTube(url)
     ys = yt.streams.get_highest_resolution()
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    ys.download(output_path=os.path.dirname(temp_file.name), filename=os.path.basename(temp_file.name))
-    return yt.title, temp_file.name
+    download_path = ys.download(path)
+    return yt.title, download_path
 
 @app.route('/')
 def index():
@@ -21,11 +13,18 @@ def download():
     data = request.get_json()
     url = data['url']
 
-    # Supondo que você tenha uma função download_video que baixa o vídeo e retorna o título e caminho
-    title, full_download_path = download_video(url)
+    # Use um diretório temporário para downloads
+    download_path = '/tmp/downloads'
 
-    # Enviar o arquivo como um download para o cliente
-    return send_file(full_download_path, as_attachment=True, download_name=f"{title}.mp4")
+    # Certifique-se de que o diretório de downloads exista
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    # Supondo que você tenha uma função download_video que baixa o vídeo e retorna o título e caminho
+    title, full_download_path = download_video(url, download_path)
+
+    message = f'<span class=\'txt_vermelho\'>Video</span><span class=\'txt_laranja\'>"{title}"</span> foi baixado <span class=\'txt_ciano\'>com sucesso!</span>'
+    return jsonify({'message': message, 'download_path': full_download_path})
 
 @app.route('/get_thumbnail', methods=['POST'])
 def get_thumbnail():
