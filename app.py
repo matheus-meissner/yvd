959a6d215bc2dc7ee79c92ed5544234cf19500da
Matheus Meissner <matheus.iembo@gmail.com>
2024-06-30 21:00:32 -0300

    teste_commit_1

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from pytube import YouTube
+from flask import Flask, request, jsonify, render_template, send_file, after_this_request
 import os
-from flask import Flask, request, jsonify, render_template
+from pytube import YouTube
 
 app = Flask(__name__)
 
 def download_video(url, path):
     yt = YouTube(url)
     ys = yt.streams.get_highest_resolution()
-    ys.download(path)
-    return yt.title
+    video_path = ys.download(path)
+    return yt.title, video_path
 
 @app.route('/')
 def index():
@@ -18,18 +18,30 @@ def index():
 def download():
     data = request.get_json()
     url = data['url']
-    # Determinar o caminho de downloads padrão do sistema operacional
-    home = os.path.expanduser("~")
+    home = os.path.expanduser("~")  # Determinar o caminho de downloads padrão do sistema operacional
     if os.name == 'nt':  # Windows
         download_path = os.path.join(home, 'Downloads')
     else:  # macOS, Linux, etc.
         download_path = os.path.join(home, 'Downloads')
-        # Certificar que o diretório de downloads existe
+    
+    # Certificar que o diretório de downloads existe
     if not os.path.exists(download_path):
         os.makedirs(download_path)
-    title = download_video(url, download_path)
+
+    title, video_path = download_video(url, download_path)
+
+    # Enviar o arquivo como anexo para o navegador fazer o download
+    response = send_file(video_path, as_attachment=True, download_name=f"{title}.mp4")
+
+    # Adicionar cabeçalhos para forçar o download
+    response.headers["Content-Disposition"] = f"attachment; filename={title}.mp4"
+    response.headers["Content-Type"] = "application/octet-stream"
+    
+    # Adicionar mensagem à resposta
     message = f'<span class=\'txt_vermelho\'>Video</span><span class=\'txt_laranja\'>"{title}"</span> foi baixado <span class=\'txt_ciano\'>com sucesso!</span>'
-    return jsonify({'message': message})
+    response.headers["X-Download-Message"] = message
+    
+    return response
 
 @app.route('/get_thumbnail', methods=['POST'])
 def get_thumbnail():
